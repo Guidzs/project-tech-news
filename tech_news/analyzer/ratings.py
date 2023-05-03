f365c27fcfe852e727d4bd2cae74bccd22956dd2
Guilherme Borges <guidz.2004@gmail.com>
2023-05-03 19:38:25 -0300

    feat: implementa função que lista as categorias

--- tech_news/analyzer/ratings.py
+++ tech_news/analyzer/ratings.py
@@ -1,3 +1,9 @@
+from collections import Counter
+from tech_news.database import find_news
+
+
 # Requisito 10
 def top_5_categories():
-    """Seu código deve vir aqui"""
+    categories = sorted([news["category"] for news in find_news()])
+    top_5 = [c[0] for c in Counter(categories).most_common(5)]
+    return top_5
